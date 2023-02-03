5f556879e14b234b20b9f14fd61c42b6f221de78
cs-marshall <marshall.sumwalt@crowdstrike.com>
2023-02-03 15:09:56 -0600

    feat(mget): replaced mgetDoc with DocumentRef
    Renamed DocumentID to a more apt DocumentRef as it references a document in a specific location. Once doing that, it made sense to use it for the MGetRequest instead of the unexperted mgetDoc.
    
    This change also lets users to submit their own implementation of RoutableDoc if they are leveraging it.

--- document.go
+++ document.go
@@ -3,52 +3,52 @@ package opensearchtools
 import "encoding/json"
 
 // RoutableDoc interface defines an OpenSearch document that can be routed to a specific index.
-// The most basic implementation is DocumentID.
+// The most basic implementation is DocumentRef.
 type RoutableDoc interface {
-    // ID returns the document ID
-    ID() string
-    // Index returns the index the document should be routed to
-    Index() string
+	// ID returns the document ID
+	ID() string
+	// Index returns the index the document should be routed to
+	Index() string
 }
 
-// DocumentID is a simple implementation of RoutableDoc. Identifying a document with it's basic components.
-type DocumentID struct {
-    id    string
-    index string
+// DocumentRef references a document via its index and id. It is the most basic implementation of RoutableDoc
+type DocumentRef struct {
+	id    string
+	index string
 }
 
-// NewDocumentID constructs a DocumentID with the core two identifiers, ID and Index.
-func NewDocumentID(id, index string) DocumentID {
-    return DocumentID{
-        id:    id,
-        index: index,
-    }
+// NewDocumentRef constructs a DocumentRef with the core two identifiers, ID and Index.
+func NewDocumentRef(id, index string) DocumentRef {
+	return DocumentRef{
+		id:    id,
+		index: index,
+	}
 }
 
 // ID returns the document ID
-func (d DocumentID) ID() string {
-    return d.id
+func (d DocumentRef) ID() string {
+	return d.id
 }
 
 // Index returns the index the document should be routed to
-func (d DocumentID) Index() string {
-    return d.index
+func (d DocumentRef) Index() string {
+	return d.index
 }
 
 // DocumentResult defines any OpenSearch response that contains a document source.
 type DocumentResult interface {
-    // GetSource returns the raw bytes of the document
-    GetSource() []byte
+	// GetSource returns the raw bytes of the document
+	GetSource() []byte
 }
 
 // PtrTo is a generic constraint that restricts value to be pointers.
 // T can be any type.
 type PtrTo[T any] interface {
-    *T
+	*T
 }
 
 // ReadDocument reads the source from a DocumentResult and parses it into the passed document object.
 // Document and be any pointer type.
 func ReadDocument[D any, P PtrTo[D], R DocumentResult](docResult R, document P) error {
-    return json.Unmarshal(docResult.GetSource(), document)
+	return json.Unmarshal(docResult.GetSource(), document)
 }
--- mget.go
+++ mget.go
@@ -1,13 +1,13 @@
 package opensearchtools
 
 import (
-    "bytes"
-    "context"
-    "encoding/json"
-    "net/http"
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
 
-    "github.com/opensearch-project/opensearch-go/v2"
-    "github.com/opensearch-project/opensearch-go/v2/opensearchapi"
+	"github.com/opensearch-project/opensearch-go/v2"
+	"github.com/opensearch-project/opensearch-go/v2/opensearchapi"
 )
 
 // MGetRequest wraps the functionality of [opensearchapi.MgetRequest] by supporting request body creation.
@@ -17,36 +17,59 @@ import (
 //	    Add("example_index", "totally_real_id").
 //	    Do(context.background(), client)
 type MGetRequest struct {
-    // Index destination for entire request
-    // if used individual documents don't need to specify the index
-    Index string `json:"-"`
+	// Index destination for entire request
+	// if used individual documents don't need to specify the index
+	Index string
 
-    // Docs are the list of documents to be fetched
-    Docs []mgetDoc `json:"docs"`
+	// Docs are the list of documents to be fetched.
+	Docs []RoutableDoc
 }
 
 // NewMGetRequest instantiates an empty MGetRequest.
 // An empty MGetRequest is executable but will return zero documents because zero have been requested.
 func NewMGetRequest() *MGetRequest {
-    return &MGetRequest{}
+	return &MGetRequest{}
 }
 
-// Add a request for a document with id at a specific index.
-// If index is an empty string, the request relies on the top level index MGetRequest.Index
+// Add a DocumentRef to the documents being requested.
+// If index is an empty string, the request relies on the top level MGetRequest.Index.
 func (m *MGetRequest) Add(index, id string) *MGetRequest {
-    m.Docs = append(m.Docs, mgetDoc{
-        ID:    id,
-        Index: index,
-    })
+	return m.AddDocs(NewDocumentRef(index, id))
+}
 
-    return m
+// AddDocs - add any number RoutableDoc to the documents being requested.
+// If the doc does not return anything for [RoutableDoc.Index], the request relies on the top level MGetRequest.Index.
+func (m *MGetRequest) AddDocs(docs ...RoutableDoc) *MGetRequest {
+	m.Docs = append(m.Docs, docs...)
+	return m
 }
 
 // SetIndex sets the top level index for the request. If a individual document request does not have an index specified,
 // this index will be used.
 func (m *MGetRequest) SetIndex(index string) *MGetRequest {
-    m.Index = index
-    return m
+	m.Index = index
+	return m
+}
+
+// Source translates the MGetRequest into the shape expected by OpenSearch.
+func (m *MGetRequest) Source() any {
+	docs := make([]any, len(m.Docs))
+	for i, d := range m.Docs {
+		docReq := make(map[string]any)
+
+		if d.Index() != "" {
+			docReq["_index"] = d.Index()
+		}
+
+		docReq["_id"] = d.ID()
+
+		docs[i] = d
+	}
+
+	source := make(map[string]any)
+	source["docs"] = docs
+
+	return source
 }
 
 // Do executes the Multi-Get MGetRequest using the provided opensearch.Client.
@@ -56,64 +79,58 @@ func (m *MGetRequest) SetIndex(index string) *MGetRequest {
 //   - The request to OpenSearch fails
 //   - The results json cannot be unmarshalled
 func (m *MGetRequest) Do(ctx context.Context, client *opensearch.Client) (*MGetResponse, error) {
-    bodyBytes, jErr := json.Marshal(m)
-    if jErr != nil {
-        return nil, jErr
-    }
+	bodyBytes, jErr := json.Marshal(m.Source())
+	if jErr != nil {
+		return nil, jErr
+	}
 
-    osResp, rErr := opensearchapi.MgetRequest{
-        Index: m.Index,
-        Body:  bytes.NewReader(bodyBytes),
-    }.Do(ctx, client)
+	osResp, rErr := opensearchapi.MgetRequest{
+		Index: m.Index,
+		Body:  bytes.NewReader(bodyBytes),
+	}.Do(ctx, client)
 
-    if rErr != nil {
-        return nil, rErr
-    }
+	if rErr != nil {
+		return nil, rErr
+	}
 
-    resp := &MGetResponse{
-        StatusCode: osResp.StatusCode,
-        Header:     osResp.Header,
-    }
+	resp := &MGetResponse{
+		StatusCode: osResp.StatusCode,
+		Header:     osResp.Header,
+	}
 
-    var respBuf bytes.Buffer
-    if _, err := respBuf.ReadFrom(osResp.Body); err != nil {
-        return nil, err
-    }
+	var respBuf bytes.Buffer
+	if _, err := respBuf.ReadFrom(osResp.Body); err != nil {
+		return nil, err
+	}
 
-    if err := json.Unmarshal(respBuf.Bytes(), &resp); err != nil {
-        return nil, err
-    }
+	if err := json.Unmarshal(respBuf.Bytes(), &resp); err != nil {
+		return nil, err
+	}
 
-    return resp, nil
+	return resp, nil
 }
 
-// mgetDoc is a simple struct representing an individual document to be fetched in a MultiGet request
-type mgetDoc struct {
-    ID    string `json:"_id,omitempty"`
-    Index string `json:"_index,omitempty"`
-}
-
-// MGetResponse wraps the functionality of [opensearchapi.Response] by unmarshalling the reponse body into a
-// slice of MGetResults
+// MGetResponse wraps the functionality of [opensearchapi.Response] by unmarshalling the response body into a
+// slice of MGetResults.
 type MGetResponse struct {
-    StatusCode int          `json:"-"`
-    Header     http.Header  `json:"-"`
-    Docs       []MGetResult `json:"docs,omitempty"`
+	StatusCode int          `json:"-"`
+	Header     http.Header  `json:"-"`
+	Docs       []MGetResult `json:"docs,omitempty"`
 }
 
-// MGetResult is the individual result for each requested item
+// MGetResult is the individual result for each requested item.
 type MGetResult struct {
-    Index       string          `json:"_index,omitempty"`
-    ID          string          `json:"_id,omitempty"`
-    Version     int             `json:"_version,omitempty"`
-    SeqNo       int             `json:"_seq_no,omitempty"`
-    PrimaryTerm int             `json:"_primary_term,omitempty"`
-    Found       bool            `json:"found,omitempty"`
-    Source      json.RawMessage `json:"_source,omitempty"`
-    Error       error           `json:"-"`
+	Index       string          `json:"_index,omitempty"`
+	ID          string          `json:"_id,omitempty"`
+	Version     int             `json:"_version,omitempty"`
+	SeqNo       int             `json:"_seq_no,omitempty"`
+	PrimaryTerm int             `json:"_primary_term,omitempty"`
+	Found       bool            `json:"found,omitempty"`
+	Source      json.RawMessage `json:"_source,omitempty"`
+	Error       error           `json:"-"`
 }
 
-// GetSource returns the raw bytes of the document of the MGetResult
+// GetSource returns the raw bytes of the document of the MGetResult.
 func (m MGetResult) GetSource() []byte {
-    return []byte(m.Source)
+	return []byte(m.Source)
 }
