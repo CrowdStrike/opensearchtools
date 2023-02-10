837b6fb683a06ca27d56443c3a3ebda898258865
Jeff McLean <jeff.mclean@crowdstrike.com>
2023-02-10 10:37:39 -0500

    remove unused generic

--- internal/mget.go
+++ internal/mget.go
@@ -7,8 +7,6 @@ import (
 
 // MGet defines a method which knows how to make an OpenSearch multiple document get.
 // It should be implemented by a version-specific executor.
-type MGet[K opensearchtools.RoutableDoc] interface {
+type MGet interface {
 	MGet(ctx context.Context, req *opensearchtools.MGetRequest) (*opensearchtools.MGetResponse, error)
 }
-
-// TODO: define other things you can do with an executor here
