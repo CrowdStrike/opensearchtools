504db425bcf18168d84b4818cd17000c402cb16c
Jeff McLean <jeff.mclean@crowdstrike.com>
2023-02-24 14:21:12 -0500

    add missing doc comment

--- response.go
+++ response.go
@@ -2,6 +2,8 @@ package opensearchtools
 
 import "net/http"
 
+// OpenSearchResponse is a generic return type for an OpenSearch query which has meta fields common
+// to all response types as well as a generic Response field abstract across all response types.
 type OpenSearchResponse[T any] struct {
 	ValidationResults ValidationResults
 	StatusCode        int
